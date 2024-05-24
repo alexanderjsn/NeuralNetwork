92ba04cf48bfce81ac1b3a914a55811fcad60ee9
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-05-25 01:56:47 +0200

    started creating a simple NLP model, slowly advancing it to learn

--- Main.java
+++ Main.java
@@ -1,6 +1,72 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Wccorld!");
 
     }
+
+    
+
+
+
+
+
+
+
+
+
+
 }
+
+    /*public static int calculateWeights(double input_1, double input_2){
+        
+
+
+        // weights for non poisionous
+        double weight_1_1 = 0.5;
+        double weight_1_2 = 0.8;
+
+        // weight for poisionous
+        double weight_2_1 = 0.3; 
+        double weight_2_2 = 0.6; 
+
+        // bias for finetuning
+        double bias_1 = 0.2; 
+        double bias_2 = 0.4; 
+
+        // two possible outcomes
+
+        // takes input 1 * weight and input 2 * weight + a bias
+        double output_1 = input_1 * weight_1_1 + input_2 * weight_2_1 + bias_1;
+        double output_2 = input_1 * weight_1_2 + input_2 * weight_2_2 + bias_2;
+        
+            if (output_1 > output_2) {
+                System.out.println("1");
+                return 1;
+            } else {
+                System.out.println("0");
+                return 0;
+            }
+        }
+
+        
+
+        }
+
+        /* 
+        public static int[] userInput(){
+            boolean condition = true;
+            String input;
+            System.out.println("Enter the first input: ");
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine(); 
+            int[] vector = new int[input.length()];
+            for (int i = 0; i < input.length(); i++) {
+                vector[i] = (int) input.charAt(i);
+            }
+            System.out.println(vector);
+            return vector;
+        }}*/
+
+    
+
